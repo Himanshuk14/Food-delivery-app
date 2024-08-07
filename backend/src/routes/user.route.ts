9b7b81a9d4060d5a74edbbbcc4f8438a5a5d5eac
Himanshuk14 <hsrreso1234@gmail.com>
2024-08-08 01:29:23 +0530

    Add the token passing with the help of audience and middlewares in backend

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller";
+import { jwtCheck } from "../middlewares/auth.middleware";
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(jwtCheck, registerUser);
 
 export default router;
